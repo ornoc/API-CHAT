61181106bf78a341db5c7b98bed837340cb0280b
ornoc <chaenebraza2020@gmail.com>
2024-07-06 08:44:39 -0300

    Atualizações nos códigos- Rotas não dando certo (?) (nn sei bem o pq)

---
+++ src/api.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extend:true}));
+app.use(express.json());
+
+const router = express.Router();
+
+//app.use('/', router.get('/'), (req, res) =>{
+   // res.status(200).send("<h1>API-CHAT</h1>")
+//});
+
+//module.exports = app;
+
+
+app.use('/', router.get('/', (req,res, next) => {
+    res.status(200).send("<h1>API - CHAT</h1>");
+}));
+
+app.use("/",router.get("/sobre", (req, res, next) => {
+    res.status(200).send({
+        "nome":"API - CHAT",
+        "versão": "0.1.0",
+        "autor":"Cândido Farias"
+    })
+}));
+
+module.exports = app;
