6025b4ebac6b00d8f3349485be4781ea4119e3e8
Chaene <132903724+ornoc@users.noreply.github.com>
2024-07-17 07:50:02 -0300

    Update salaModel.js

--- src/models/salaModel.js
+++ src/models/salaModel.js
@@ -4,8 +4,7 @@ function listarSalas() {
     return db.findAll("salas");
 }
 
-/**
- * 
+/*
  * @param {string} nome
  * @returns 
  */
@@ -14,31 +13,3 @@ function criarSala(nome) {
 }
 
 module.exports = { listarSalas, criarSala }
-
-/* VERSAO 1 - PAGINA 7
-
-function listarSalas() {
-    return [
-        {
-            "_id": {
-                "$oid": "643ece43ea11e6e5b0421f10"
-            },
-            "nome": "Guerreiros da InfoCimol",
-            "tipo": "publica"
-        },{
-            "_id": {
-                "$oid": "643ecec1ea11e6e5b0421f12"
-            },
-            "nome": "Só os confirmados da INFO",
-            "tipo": "privada",
-            "chave": "atat8q4haw"
-        },
-        {
-            "_id": {
-                "$oid": "643f22a2ea11e6e5b0421f18"
-            },
-            "nome": "Guerreiros da INFO",
-            "tipo": "publico"
-        }
-    ];
-}*/
