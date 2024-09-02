0de525bc0fd524a05fc789e60f22774bbd2311d4
Caca <132903724+ornoc@users.noreply.github.com>
2024-09-02 14:29:44 -0300

    Update db.js

--- src/models/db.js
+++ src/models/db.js
@@ -1,29 +1,32 @@
-const { MongoClient, Db} = require("mongodb");
-
+const { MongoClient, ObjectId } = require('mongodb');
 let singleton;
 
-/*
- * Connect to the database
- * @returns {Promise<Db>}
- */
 async function connect() {
     if (singleton) return singleton;
 
-    const client = new MongoClient(process.env.DB_HOST);
+    const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
-
     singleton = client.db(process.env.DB_DATABASE);
     return singleton;
 }
 
-async function findAll(collection) {
+exports.findAll = async (collection) => {
     const db = await connect();
     return db.collection(collection).find().toArray();
-}
+};
 
-async function insertOne(collection, data) {
+exports.insertOne = async (collection, data) => {
     const db = await connect();
     return db.collection(collection).insertOne(data);
-}
+};
 
-module.exports = { findAll, insertOne }
+exports.findOne = async (collection, _id) => {
+    const db = await connect();
+    let obj = await db.collection(collection).find({ '_id': new ObjectId(_id) }).toArray();
+    return obj[0] || false;
+};
+
+exports.updateOne = async (collection, object, param) => {
+    const db = await connect();
+    return await db.collection(collection).updateOne(param, { $set: object });
+};
