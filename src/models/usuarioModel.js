f599c486af28c31dc7087b80b9db0d621d8631d4
Caca <132903724+ornoc@users.noreply.github.com>
2024-09-04 22:00:34 -0300

    Update usuarioModel.js

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,15 +1,7 @@
-const db = require('../db');
+const db = require('./db');
 
 exports.criarUsuario = async (nick) => {
-    const user = { nick, sala: null };
-    const result = await db.insertOne("usuarios", user);
-    return result.insertedId ? { _id: result.insertedId, ...user } : null;
-};
-
-exports.buscarUsuario = async (idUser) => {
-    return await db.findOne("usuarios", idUser);
-};
-
-exports.alterarUsuario = async (user) => {
-    return await db.updateOne("usuarios", user, { _id: user._id });
+  const novoUsuario = { nick };
+  await db.insertOne('usuarios', novoUsuario);
+  return novoUsuario;
 };
