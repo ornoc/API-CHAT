8e045806a4685fe878992326b6c637af953040b3
Caca <132903724+ornoc@users.noreply.github.com>
2024-09-04 22:03:12 -0300

    Update salaController.js

--- src/controllers/salaController.js
+++ src/controllers/salaController.js
@@ -1,39 +1,53 @@
-// src/controllers/salaController.js
 const salaModel = require('../models/salaModel');
 
-exports.listarSalas = async () => {
-    return await salaModel.listarSalas();
+exports.get = async (req, res) => {
+  const salas = await salaModel.listarSalas();
+  res.status(200).json(salas);
 };
 
-exports.entrarNaSala = async (iduser, idsala) => {
-    const sala = await salaModel.buscarSala(idsala);
-    let usuarioModel = require('../models/usuarioModel');
-    let user = await usuarioModel.buscarUsuario(iduser);
-
+  exports.entrar = async (idUser, idSala) => {
+    const sala = await salaModel.buscarSala(idSala);
+    const usuarioModel = require('../models/usuarioModel');
+    const user = await usuarioModel.buscarUsuario(idUser);
+  
     user.sala = { _id: sala._id, nome: sala.nome, tipo: sala.tipo };
+    await usuarioModel.alterarUsuario(user);
+  
+    return { msg: "Entrou na sala", timestamp: Date.now() };
+  };
 
-    if (await usuarioModel.alterarUsuario(user)) {
-        return { msg: "OK", timestamp: Date.now() };
+  exports.criarSala = async (nome, tipo) => {
+    const novaSala = { nome, tipo, msgs: [] };
+    await salaModel.criarSala(novaSala);
+    return novaSala;
+
+};
+    exports.enviarMensagem = async (nick, msg, idSala) => {
+        const sala = await salaModel.buscarSala(idSala);
+        const timestamp = Date.now();
+      
+        if (!sala.msgs) sala.msgs = [];
+        sala.msgs.push({ timestamp, msg, nick });
+      
+        await salaModel.atualizarMensagens(sala);
+        return { msg: "Mensagem enviada", timestamp };
+      
+  };
+
+  exports.buscarMensagens = async (idSala, timestamp) => {
+    const mensagens = await salaModel.buscarMensagens(idSala, timestamp);
+    return { timestamp: mensagens[mensagens.length - 1]?.timestamp || null, msgs: mensagens };
+  };
+  
+  exports.sair = async (idUser, idSala) => {
+    const usuarioModel = require('../models/usuarioModel');
+    const user = await usuarioModel.buscarUsuario(idUser);
+  
+    if (user.sala && user.sala._id === idSala) {
+      delete user.sala;
+      await usuarioModel.alterarUsuario(user);
     }
-
-    return false;
-};
-
-exports.enviarMensagem = async (nick, msg, idsala) => {
-    const sala = await salaModel.buscarSala(idsala);
-
-    if (!sala.msgs) {
-        sala.msgs = [];
-    }
-
-    const timestamp = Date.now();
-    sala.msgs.push({ timestamp, msg, nick });
-
-    let resp = await salaModel.atualizarMensagens(sala);
-    return { msg: "OK", timestamp };
-};
-
-exports.buscarMensagens = async (idsala, timestamp) => {
-    let mensagens = await salaModel.buscarMensagens(idsala, timestamp);
-    return { timestamp: mensagens[mensagens.length - 1].timestamp, msgs: mensagens };
-};
+  
+    return { msg: "Saiu da sala" };
+  };
+  
