d42d5f3f7d2757a4e34a3380e0a7be642c525f44
Caca <132903724+ornoc@users.noreply.github.com>
2024-09-04 22:00:08 -0300

    Update salaModel.js

--- src/models/salaModel.js
+++ src/models/salaModel.js
@@ -1,23 +1,14 @@
 // src/models/salaModel.js
-const db = require('../db');
+const db = require('./db');
 
 exports.listarSalas = async () => {
-    return await db.findAll("salas");
+  return await db.findAll('salas');
 };
 
-exports.buscarSala = async (idsala) => {
-    return db.findOne("salas", idsala);
+exports.buscarSala = async (idSala) => {
+  return await db.findOne('salas', idSala);
 };
 
-exports.atualizarMensagens = async (sala) => {
-    return await db.updateOne("salas", sala, { _id: sala._id });
-};
-
-exports.buscarMensagens = async (idsala, timestamp) => {
-    let sala = await this.buscarSala(idsala);
-    if (sala.msgs) {
-        let msgs = sala.msgs.filter(msg => msg.timestamp >= timestamp);
-        return msgs;
-    }
-    return [];
+exports.criarSala = async (sala) => {
+  return await db.insertOne('salas', sala);
 };
